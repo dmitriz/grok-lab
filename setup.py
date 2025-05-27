8186720f4c5b2093e3ede368b41a643625bc37ac
Dmitri Zaitsev <dmitri14@gmail.com>
2025-05-27 20:29:21 +0700

    h

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ Package setup configuration for grok-lab
 """
 
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of README.md
+this_directory = Path(__file__).parent
+with open(this_directory / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
   name="grok-lab",
@@ -10,7 +16,7 @@ setup(
   author="Dmitri Zaitsev",
   author_email="dmitri14@gmail.com",
   description="A client library for interacting with Grok-3 API",
-  long_description=open("README.md").read(),
+  long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/dmitriz/grok-lab",
   packages=find_packages(),
